c08aefe92003bf091b67357e22716831f152db01
sanchagrins <sanchagrins@gmail.com>
2017-08-17 23:58:09 +0900

    UI tweaks

--- slack_invite/slack_invite/forms.py
+++ slack_invite/slack_invite/forms.py
@@ -2,3 +2,4 @@ from django import forms
 
 class InviteForm(forms.Form):
     email_addr = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'Email address','id':'email'}), label='')
+    check_terms = forms.BooleanField(label="I agree to the <a link''>Terms and Code of Conduct</a>")
