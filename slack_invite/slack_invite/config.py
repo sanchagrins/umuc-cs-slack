81667dba5c296740c342cb1161a69fe528d9b477
sanchagrins <sanchagrins@gmail.com>
2017-08-17 14:01:32 +0900

    removed debugging statements

--- slack_invite/slack_invite/config.py
+++ slack_invite/slack_invite/config.py
@@ -1,8 +1,3 @@
-# ----------------------
-# Configuration file for the Django-SlackInvite App. Set your community name and
-# slack url here.
-# ----------------------
-
 import os
 import configparser
 
@@ -14,10 +9,8 @@ def get_env_variable(var_name):
 
 path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 config = configparser.ConfigParser()
-print(path+'/settings.cfg')
 config.read(path + '/settings.cfg')
 
 slack_url = get_env_variable('SLACK_URL')
-print(slack_url)
 slack_team = get_env_variable('SLACK_TEAM')
 slack_token = get_env_variable('SLACK_TOKEN')
